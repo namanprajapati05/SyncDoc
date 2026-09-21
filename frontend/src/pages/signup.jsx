dfba65d60d88a31f56f97cf9ac551b89ab2933b1
27pavan <naveenpavan765@gmail.com>
2026-09-21 15:23:09 +0530

    Add Signup component for user registration

---
+++ frontend/src/pages/signup.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Signup() {
+  return (
+    <div>
+      <h1>Signup</h1>
+
+      <input type="text" placeholder="Name" />
+      <br />
+
+      <input type="email" placeholder="Email" />
+      <br />
+
+      <input type="password" placeholder="Password" />
+      <br />
+
+      <button>Signup</button>
+    </div>
+  );
+}
+
+export default Signup;
