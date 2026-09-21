27f898e9065f36a3220c98f87b7f2cf31d3c9692
27pavan <naveenpavan765@gmail.com>
2026-09-21 15:21:50 +0530

    Add Login component with email and password fields

---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <input type="email" placeholder="Email" />
+      <br />
+
+      <input type="password" placeholder="Password" />
+      <br />
+
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Login;
