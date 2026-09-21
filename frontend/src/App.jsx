c5429c8f6fff6dccccadc0dbcc464e1711ca7f99
27pavan <naveenpavan765@gmail.com>
2026-09-21 15:26:58 +0530

    Add signup/login toggle functionality to App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
 
 const App = () => {
+  const [showSignup, setShowSignup] = useState(false);
+
   return (
     <div>
-      
-    </div>
-  )
-}
+      {showSignup ? (
+        <Signup />
+      ) : (
+        <Login />
+      )}
 
-export default App
+      <button onClick={() => setShowSignup(!showSignup)}>
+        {showSignup ? "Go to Login" : "Go to Signup"}
+      </button>
+    </div>
+  );
+};
+
+export default App;
